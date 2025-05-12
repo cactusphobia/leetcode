e8928fe19900959ff4921baea92d208ae66a3cf2
ThinhLe <tm.lequocthinh@gmail.com>
2025-05-12 17:27:42 +0700

    Add Leetcode 1480: Running Sum of 1 Array

---
+++ tibim/sum_array.py
@@ -0,0 +1,9 @@
+nums01 = [1, 2, 3, 4]
+nums02 = [1, 1, 1, 1, 1]
+nums03 = [3, 1, 2, 10,1]
+
+def tinhtong(nums):
+    for i in range(1, len(nums)):
+        nums[i] = nums[i] + nums[i-1]
+    return nums
+print(tinhtong(nums01))
